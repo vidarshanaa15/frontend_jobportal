618a4cd0ddee98c18cf4f61ce060a2d384d2fbad
vidarshanaa15 <vidarshanaa15@gmail.com>
2024-10-31 15:31:11 +0530

    went for lunch

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -4,12 +4,35 @@ const HeaderComponent = () => {
     return (
         <div>
             <header>
-                <nav className="navbar navbar-dark bg-dark fw-bold">
-                    <a className='navbar-brand' href='/'>Job Application Portal</a>
+                <nav className="navbar navbar-dark bg-dark fw-bold text-center bg-black">
+                    <a className='navbar-brand mx-auto fs-3' href='/'>Job Application Portal</a>
+                </nav>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item me-lg-5">
+                                <a className="nav-link active" href="/">Home</a>
+                            </li>
+                            <li className="nav-item me-lg-5">
+                                <a className="nav-link" href="/about">About Us</a>
+                            </li>
+                            <li className="nav-item me-lg-5">
+                                <a className="nav-link" href="/search">Search Jobs</a>
+                            </li>
+                            <li className="nav-item me-lg-5">
+                                <a className="nav-link" href="/contact">Contact Us</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/signout">Sign Out</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </header>
         </div>
-    )
+    );
 }
 
 export default HeaderComponent
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const HomeComponent = () => {
     return (
-        <div>HomeComponent</div>
-    )
+        <div className="container mt-5">
+            <div className="col-md-8 mx-auto">
+                <div className="card">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img
+                                src="https://i.ibb.co/25ZkXNt/about.jpg"
+                                alt="About us"
+                                className="img-fluid rounded-start w-75"
+                            />
+                        </div>
+                        <div className="col-md-8 d-flex flex-column justify-content-center">
+                            <div className="card-body">
+                                <h5 className="card-title">About Us</h5>
+                                <p className="card-text">
+                                    Welcome to Job Application Portal, your one-stop destination for job seekers and employers alike. We connect talented professionals with top companies and make it easy to manage applications all in one place
+                                </p>
+                                <a href="/about" className="btn btn-primary">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default HomeComponent
